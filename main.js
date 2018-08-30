75126b53792ef7aa89e5366593990687f6ae846d
Jim <42780151+beamhouse@users.noreply.github.com>
2018-08-29 22:42:10 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 window.onload = function() {
 
+    // If user is not connected via https, correct that
+    if (window.location.href.replace(/^http:\/\//i, 'https://'));
+
     // Append "Beam.House | " to the title currently given on the page
     document.title = "Beam.House | " + document.title;
 
